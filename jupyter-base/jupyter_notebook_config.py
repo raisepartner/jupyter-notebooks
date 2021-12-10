55a6728cc0a6a315bcae2c4e11bf03bdcc0f6bcf
Vincent Pfister <vincent.pfister@raisepartner.com>
2021-12-10 23:26:04 +0100

    upgrade imges to v1.1.0
    node -> v16
    jupyter lab -> v3.2.4
    add plotly

--- jupyter-base/jupyter_notebook_config.py
+++ jupyter-base/jupyter_notebook_config.py
@@ -8,8 +8,10 @@ import errno
 import stat
 
 c = get_config()  # noqa: F821
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8888
+c.ServerApp.ip = '0.0.0.0'
+c.ServerApp.port = 8888
+# c.NotebookApp.ip = '0.0.0.0'
+# c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
 
 # https://github.com/jupyter/notebook/issues/3130
